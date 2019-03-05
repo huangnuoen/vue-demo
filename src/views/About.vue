5284731a59073f6881a0acb1f6e4e757b7d218ef
huangnuoen <huangnuoen@gmail.com>
2019-03-05 17:55:09 +0800

    响应式原理

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,12 +1,34 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h2 v-if="flag">{{msg}}</h2>
+    <h2 v-else>{{msg1}}</h2>
+    <button @click="change">change</button>
+    <button @click="toggle">toggle</button>
   </div>
 </template>
 <script>
 export default {
+  data() {
+    return {
+      // dad: {
+      //   child: 'i am child'
+      // },
+      flag: true,
+      msg: "hello",
+      msg1: "world"
+    };
+  },
   beforeCreate() {
     console.log("about create");
+  },
+  methods: {
+    change() {
+      this.msg = Math.random();
+    },
+    toggle() {
+      this.flag = !this.flag;
+    }
   }
 };
 </script>
